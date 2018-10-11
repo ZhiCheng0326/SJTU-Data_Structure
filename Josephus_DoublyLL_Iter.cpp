232b3b4318a57e7bcc2895db918db1f48ebf09b0
ZhiCheng0326 <leezhixin2001@gmail.com>
2018-10-11 12:42:31 +0800

    Josephus_DoublyLL_Iter.cpp added

---
+++ Josephus_DoublyLL_Iter.cpp
@@ -0,0 +1,98 @@
+//Josephus prob solved by Circular Doubly Linked List with iterator.
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    Node *next;
+    Node *prev;
+    int data;
+};
+
+class Itr
+{
+     private:
+        Node  *current;
+
+     public:
+        Itr(Node *p) {current = p;}
+        Itr(const Itr &c){current = c.current;}         //copy constructor
+        Node *getNode(){return current;}                //return current node pointing by pointer
+        void operator++() {current = current->next; }   //for iteration
+};
+
+class cirDoublyLinkedList
+{
+    public:
+        cirDoublyLinkedList(){this->head = NULL; this->rear = NULL;};
+        void create(int d);                         //create a list of data
+        void removeNode(Node *temp);                //remove the given Node
+        Itr start() {return Itr(head);}
+        ~cirDoublyLinkedList(){delete lastNode;};
+        Node *lastNode;
+
+    private:
+        int length = 0;
+        Node *head;
+        Node *rear;
+};
+
+void cirDoublyLinkedList::create(int d)     //create a circle of d number elements.
+{
+    Node *temp;
+    for(int i=1; i<=d; ++i){
+        temp = new Node;
+        temp->data = i;
+        if(head == NULL){
+            head = temp;
+            rear = temp;
+        }
+        else{
+            temp->prev = rear;
+            rear->next = temp;
+            rear = temp;
+        }
+        ++length;
+    }
+    rear->next = head;  //connect tail to head
+    head->prev = rear;  //connect head to tail
+}
+
+void cirDoublyLinkedList::removeNode(Node *temp)
+{
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    --length;
+    delete temp;
+}
+
+int Josephus(cirDoublyLinkedList &lA, int len)
+{
+    Itr p = lA.start();
+    int counter = 1;
+    while(len>1){
+        ++p;
+        ++counter;
+        if(counter == 3){
+            Node *temp = p.getNode()->next;
+            Itr q = temp;                           //store the next node
+            lA.removeNode(p.getNode());             //remove the 3rd node
+            --len;
+            p = q;
+            counter = 1;
+        }
+    }
+    lA.lastNode = p.getNode();                      //keep the lastNode to be deleted by destructor
+    return p.getNode()->data;
+}
+
+int main()
+{
+    int n = 20;
+    cirDoublyLinkedList ListA;
+    ListA.create(n);
+    int ans = Josephus(ListA,n);    //conduct the Josephus process
+    cout << ans;                    //output the last person left in the Josephus circle
+}
